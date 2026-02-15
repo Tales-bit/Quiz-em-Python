4a3894280f6aad3fc86d80882ccac633a0fb2986
Tales <talesruvio@gmail.com>
2026-02-15 17:22:58 -0300

    V2

--- quiz.py
+++ quiz.py
@@ -1,25 +1,29 @@
-respostas = []
+pontos = []
 for i in range (0, 1):
     print("====================================")
     print("    Quiz de Matematica em Python")
     print("====================================\n")
     print("Para comecarmos, digite a quantidade de jogadores:", end=" ")
     nj = int(input())
+    for i in range(nj):
+        pontos.append(0)
     print("\n")
 
     with open("perguntas.txt", "r") as arq1, open("gabarito.txt", "r") as arq2:
         # O "3" é o total de questões 
-        for i in range(3):
-            for i in range(8):
+        for _ in range(3):
+            respostas = []
+            for _ in range(8):
                 linha = arq1.readline().rstrip()
                 print(linha)
-            for i in range(0, nj):
+            for _ in range(nj):
                 n = input()
                 respostas.append(n)
 
-            # O "3" é o total de respostas
-            for i in range(3):
-                for i in range(2):
-                    linha = arq2.readline().rstrip()
-                    for i in range(0, nj):
-                        if respostas[nj] == linha
+            # lê só a resposta da questão atual
+            arq2.readline() # ignora "Questao X"
+            linha = arq2.readline().strip().upper()
+            for i in range(nj):
+                if respostas[i].upper() == linha:
+                    print("jogador {} acertou!".format(i+1))
+                    pontos[i] = pontos[i]+1
